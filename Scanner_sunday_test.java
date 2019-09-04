4672849e57ce8b0988ede97327743bea047910e1
AnoopTiwari <53564583+AnooTiwari@users.noreply.github.com>
2019-09-04 10:42:04 +0530

    Update Scanner_sunday_test.java

--- Scanner_sunday_test.java
+++ Scanner_sunday_test.java
@@ -1,29 +1,17 @@
-
-
 import java.util.*;
 
 public class Scanner_sunday_test {
    public static void main(String[] args) {
        int x;
-       String a,b,c,e;
+       String s,a,b,c,d;
 
-   String s = "Anoop Tiwari 3 3.0 ";
-
-
-      Scanner scanner = new Scanner(s);
-      a=scanner.next();
+      Scanner scanner = new Scanner(System.in);
+      x=scanner.nextInt();
       b=scanner.next();
       c=scanner.next();
+      scanner.nextLine();
+      d=scanner.nextLine();
+      System.out.println("x--"+x+"b--"+b+"c--"+c+"d--"+d);
 
-      e=scanner.next();
-      f=scanner.next();
-      g=scanner.next();
-      System.out.println("" + a);
-      System.out.println("" + b);
-      System.out.println("" +  (Integer.parseInt(c)+1));
-
-      System.out.println("" + Float.parseFloat(e));
-
-      scanner.close();
    }
 }
